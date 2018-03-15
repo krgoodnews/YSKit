596f50812367c57531b28a4b89bacf211e22e2ca
guinnesswift <krgoodnews@gmail.com>
2018-03-15 19:48:43 +0900

    delete Project, only source

--- UIView+.swift
+++ UIView+.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 extension UIView {
+	
+	/// draw border in view to easily
 	func drawBorder(_ color: UIColor = UIColor.gray, width: CGFloat = 1) {
 		self.layer.borderColor = color.cgColor
 		self.layer.borderWidth = width
